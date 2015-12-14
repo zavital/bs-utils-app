fef518824202f59b5054671462ec6eb46a98c615
zemer <zemer@bidspirit.com>
2015-12-14 11:55:14 +0200

    z

--- app/main/utilsAppMainController.js
+++ app/main/utilsAppMainController.js
@@ -18,10 +18,28 @@ define(['./utilsAppMainModule'], function (module) {
           		$rootScope.debug("debug init");
           	}
              
+             function calendarTest(){
+        		 try {
+        			 var startDate = new Date(2016,0,5,18,30,0,0,0); // beware: month 0 = january, 11 = december
+        			 var endDate = new Date(2016,0,5,19,30,0,0,0);
+        			 var title = "My nice event";
+        			 var eventLocation = "Home";
+        			 var notes = "Some notes about this event.";
+        			 var success = function(message) { $rootScope.debug("Success: " + JSON.stringify(message)); };
+        			 var error = function(message) { $rootScope.debug("Error: " + message); };
+        			 window.plugins.calendar.createEvent(title,eventLocation,notes,startDate,endDate,success,error);
+        			 
+        		 } catch (e){
+        			 $rootScope.debug("failed to set calendar:"+e.message);
+        		 }
+            	 
+             }
+             
             function init(){
             	initDebug();
             	$scope.loadState = "loaded";
                 document.getElementById("pagePreLoader").remove();
+                calendarTest();
             }
             
             init();
